64d8932a707b5c9ebfe3e39af7512dd43e8de0e0
Sarah Brightman <sarah.grciela.brightman@gmail.com>
2023-02-16 14:02:23 -0600

    fixing my mistakes

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,8 +1,17 @@
-let oneThrough50=prompt(`please enter a number between 1 and 50`);
-for(i=0;i<50;i+=2){
-    if(oneThrough50==='true'){
+//
+    let input =parseFloat(prompt(`please enter a odd number between 1 and 50`));
+    // let isEven= (input%2===0);
+while(true){
+    if( input % 2 === 0) {
+        input = parseFloat(prompt(`please enter a odd number between 1 and 50`));
+    }else  {
+        for (let j = 1; j < 51; j += 2) {
+            if (input === j) {
+                continue;
+            }
+                console.log(`here is a odd number: ${j}`)
+
+        }
         break;
-    }else{
-        continue;
     }
 }
