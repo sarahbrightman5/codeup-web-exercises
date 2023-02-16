9c165711f560283d8cbdeaa21820c40f507588b5
Sarah Brightman <sarah.grciela.brightman@gmail.com>
2023-02-16 09:10:08 -0600

    fixing my mistakes

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,2 +1,8 @@
 let oneThrough50=prompt(`please enter a number between 1 and 50`);
-for()
+for(i=0;i<50;i+=2){
+    if(oneThrough50==='true'){
+        break;
+    }else{
+        continue;
+    }
+}
